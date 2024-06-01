cf0d1ef3236c73e289f041918a0b91ae64d396ca
Pusaka Manggala <pusakamanggala@gmail.com>
2024-06-01 15:04:08 +0700

    ref : contact data

---
+++ src/Contact/contactData.tsx
@@ -0,0 +1,10 @@
+import { Contact } from "@/types/contact";
+
+const contactData: Contact = {
+  address: "JL. Prajurit KKO Usman dan Harun Senen Jakarta Pusat",
+  whatsapp: "085156842949",
+  email: "cs@.......(domain pt)",
+  phone: "0213506550",
+};
+
+export default contactData;
---
+++ src/types/contact.ts
@@ -0,0 +1,6 @@
+export type Contact = {
+  address: string;
+  whatsapp: string;
+  email: string;
+  phone: string;
+};
